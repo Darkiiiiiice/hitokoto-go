b51c6540d61d37d913681b0c306ccbec795ad11c
MarioMang <853275286@qq.com>
2023-03-25 23:01:27 +0800

    [update]: add all api constants

--- execute_test.go
+++ execute_test.go
@@ -8,7 +8,8 @@ import (
 	"github.com/mariomang/hitokoto/op"
 )
 
-func TestDoForLogin(t *testing.T) {
+// TestDoForLoginSuccess This test case tests the successful login scenario
+func TestDoForLoginSuccess(t *testing.T) {
 	e := NewExecutor("token")
 
 	req := &op.LoginRequest{
@@ -22,6 +23,30 @@ func TestDoForLogin(t *testing.T) {
 	}
 	if resp.ID != 11628 {
 		t.Errorf("ID is not correct: %v", resp.ID)
+	}
+
+	fmt.Printf("resp: %v", resp)
+}
+
+// TestDoForLoginFailed This test case tests the failed login scenario
+func TestDoForLoginFailed(t *testing.T) {
+	e := NewExecutor("token")
+
+	req := &op.LoginRequest{
+		Email:    "user@hitokoto.cn",
+		Password: "xxxxxxxxx",
+	}
+	resp := &op.LoginResponse{}
+	err := e.Do(&constants.APILogin, req, resp)
+	if err != nil {
+		e, ok := err.(*HitokotoError)
+		if !ok {
+			t.Errorf("Error executing request: %v", err)
+		}
+
+		if e.Status != -1 {
+			t.Errorf("Status is not correct: %v", e.Status)
+		}
 	}
 
 	fmt.Printf("resp: %v", resp)
